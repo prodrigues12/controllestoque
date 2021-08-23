62a94ae011c047a3707865c39a9483d5e930b5e9
prodrigues12 <paulinorn12@gmail.com>
2021-08-23 05:51:40 -0300

    login

---
+++ controlestoque/src/main/java/com/controlestoque/security/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.controlestoque.security;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer {
+	
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/lista-produto").setViewName("/produto/listaProduto");
+	}
+
+}
