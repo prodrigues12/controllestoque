ea2b4bc52758c2a324f92635c0257d8dfbed74d4
prodrigues12 <paulinorn12@gmail.com>
2021-11-28 06:58:05 -0200

    Refazendo

--- controlestoque/src/main/java/com/controlestoque/controller/ProdutoController.java
+++ controlestoque/src/main/java/com/controlestoque/controller/ProdutoController.java
@@ -12,118 +12,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.controlestoque.Repository.GrupoRepository;
-import com.controlestoque.Repository.ItemRepository;
+
 import com.controlestoque.Repository.ProdutoRepository;
-import com.controlestoque.Repository.SecaoRepository;
-import com.controlestoque.model.Grupo;
+
 
 import com.controlestoque.model.Produto;
-import com.controlestoque.model.Secao;
+
 
 @Controller
+@RequestMapping("/produto")
 public class ProdutoController {
-	
+
 	@Autowired
 	ProdutoRepository pr;
-	
-	@Autowired
-	SecaoRepository sr;
-	
-	@Autowired
-	ItemRepository ir;
-	
-	@Autowired
-	GrupoRepository gr;
-	
-	
-	@GetMapping("/lista-produto")
-	public ModelAndView listProdutos() {
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("produto/listaProduto");
-		mv.addObject("produtoList" ,pr.findAll());
-		
-		return mv;
-	}
-	
-	@GetMapping("/inserirProduto")
-	public ModelAndView insertProduto() {
+
+	@RequestMapping("/novo")
+	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("produto/formProduto");
-		mv.addObject("produto", new Produto());
-		
-		Iterable<Secao> secao = sr.findAll();
-		mv.addObject("secaoList", secao);
-		
-		Iterable<Grupo> grupo = gr.findAll();
-		mv.addObject("grupoList", grupo);
-		
 		return mv;
-		
 	}
-
-	@PostMapping("salvar-Produto")
-	public ModelAndView insertProduto(@Validated Produto produto, BindingResult br) {
-		ModelAndView mv = new ModelAndView();
-		if(br.hasErrors()) {
-			mv.setViewName("produto/formProduto");
-			mv.addObject("produto");
-	}else {
-		mv.setViewName("redirect:/lista-produto");
-		pr.save(produto);
-	}
+	
+	@RequestMapping("/menu")
+	public ModelAndView menu() {
+		ModelAndView mv = new ModelAndView("fragmentos/fragmentos");
 		return mv;
 	}
 
-	
-	
-	@GetMapping("{idProduto}")
-	public ModelAndView alterar(@PathVariable("idProduto") Long idProduto) {
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("produto/alterarProduto");
-		Produto produto = pr.getById(idProduto);
-		
-//		Produto pro = pr.findOne(idProduto);
-		
-		mv.addObject("produto", produto);
-		Iterable<Secao> secao = sr.findAll();
-		mv.addObject("secaoList", secao);
-		Iterable<Grupo> grupo = gr.findAll();
-		mv.addObject("grupoList", grupo);
-		
-		return mv;
-		
-	}
-	
-	@PostMapping("/alterar")
-	public ModelAndView alterar(Produto produto) {
-		ModelAndView mv = new ModelAndView();
-		pr.save(produto);
-		mv.setViewName("redirect:/lista-produto");
-		return mv;
-	}
-	
-	@GetMapping("/excluir-produto/{idProduto}")
-	public String excluirProduto(@PathVariable("idProduto") Long id) {
-		pr.deleteById(id);
-		return "redirect:/lista-produto";
-	}
-	
-	@GetMapping("produtoNome")
-	public ModelAndView listaNomeProduto() {
-		ModelAndView mv = new ModelAndView("produto/pesquisaProduto");
-		return mv;
-	}
-	
-	@PostMapping("/pesquisa-produto")
-	public ModelAndView pesquisaProduto(@RequestParam("nomepesquisa") String nomepesquisa) {
-		ModelAndView mv = new ModelAndView("produto/pesquisaProduto");
-		mv.addObject("produtoListResult", pr.findByNomeContainingIngnoreCase(nomepesquisa));
-		mv.addObject("produto", new Produto());
-		return mv;
-		
-	}
-		
-
-		 
-	
 }
--- controlestoque/src/main/java/com/controlestoque/model/Produto.java
+++ controlestoque/src/main/java/com/controlestoque/model/Produto.java
@@ -1,17 +1,31 @@
 package com.controlestoque.model;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.NumberFormat;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+
 
 @Entity
 public class Produto implements Serializable{
@@ -22,27 +36,37 @@ public class Produto implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idProduto;
 	
-	@Size(min = 7, max = 40, message = "Nome do PRODUTO deve contar no minimo 7 caracteres no max 40")
-	@NotEmpty
-	@NotNull
+//	@Size(min = 7, max = 40, message = "Nome do PRODUTO deve contar no minimo 7 caracteres no max 40")
+	@NotBlank
 	private String nome;
 	
 	private String descricao;
 	
-	private int qtdEstoque;
+	@NumberFormat (pattern = "#,##0.00")
+	private BigDecimal qtdEstoque;
 	
-//	@Size(min = 0, message = "VALOR ESTOQUE MINIMO não pode ser menor que 0.")
-	private int qtdEstMin;
 
-	@ManyToOne
-	private Secao secoes;
+//	@NotNull(message = "O unidade de medida é obrigatório")
+//	@ManyToOne
+//	@JoinColumn(name = "codigo_estilo")
+//	private UnidadeMedida unidadeMedia;
 	
-	@ManyToOne
-	private Grupo grupos;
+//	@NotNull(message = "Campo é obrigatória")
+	@DecimalMin(value = "1.0", message = "Estoque deve ser no mínimo 1")
+	@NumberFormat (pattern = "#,##0.00")
+	private BigDecimal qtdEstMin;
+
+	@NotNull(message = "A seção é obrigatório")
+//	@ManyToOne
+//	@JoinColumn(name = "codigo_secao")
+//	private Secao secoes;
+//	
+//	@ManyToOne
+//	private Grupo grupos;
 	
-	@ManyToOne
-	private Rua ruas;
-	
+//	@JsonIgnore
+//	@Embedded
+//	private Endereco endereco;
 	
 //	## Construtor
 	
@@ -50,120 +74,15 @@ public class Produto implements Serializable{
 		
 	}
 
-	
-	
-public Produto(Long idProduto,
-		@Size(min = 7, max = 40, message = "Nome do PRODUTO deve contar no minimo 7 caracteres") @NotEmpty @NotNull String nome,
-		String descricao, int qtdEstoque, int qtdEstMin, Secao secoes, Grupo grupos, Rua ruas) {
-	super();
-	this.idProduto = idProduto;
-	this.nome = nome;
-	this.descricao = descricao;
-	this.qtdEstoque = qtdEstoque;
-	this.qtdEstMin = qtdEstMin;
-	this.secoes = secoes;
-	this.grupos = grupos;
-	this.ruas = ruas;
-}
-
 
 //	## Get's and Set's
 
 
-	public Long getIdProduto() {
-		return idProduto;
-	}
-
-	public void setIdProduto(Long idProduto) {
-		this.idProduto = idProduto;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-	public int getQtdEstoque() {
-		return qtdEstoque;
-	}
-
-	public void setQtdEstoque(int qtdEstoque) {
-		this.qtdEstoque = qtdEstoque;
-	}
 	
-	public int getQtdEstMin() {
-		return qtdEstMin;
-	}
-
-	public void setQtdEstMin(int qtdEstMin) {
-		this.qtdEstMin = qtdEstMin;
-	}
-
-	public Secao getSecoes() {
-		return secoes;
-	}
-
-	public void setSecoes(Secao secoes) {
-		this.secoes = secoes;
-	}
-
-	public Grupo getGrupos() {
-		return grupos;
-	}
-
-	public void setGrupos(Grupo grupos) {
-		this.grupos = grupos;
-	}
-	
-	public Rua getRuas() {
-		return ruas;
-	}
-
-	public void setRuas(Rua ruas) {
-		this.ruas = ruas;
-	}
 
 //	## hasCode and equals
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
-		return result;
-	}
-
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Produto other = (Produto) obj;
-		if (descricao == null) {
-			if (other.descricao != null)
-				return false;
-		} else if (!descricao.equals(other.descricao))
-			return false;
-		return true;
-	}
 	
-
 	
 	
 
