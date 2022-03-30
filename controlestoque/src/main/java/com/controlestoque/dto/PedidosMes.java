2e9b2c3c1caa7e335b5851ec800a42244d799ef2
prodrigues12 <paulinorn12@gmail.com>
2022-03-30 05:54:27 -0300

    Revert "Revert "Merge branch 'prodrigues12' of https://github.com/prodrigues12/controllestoque into prodrigues12""
    This reverts commit ef058ec0fbe6ab73250adf7e396fd7e9c1889ef4.

---
+++ controlestoque/src/main/java/com/controlestoque/dto/PedidosMes.java
@@ -0,0 +1,34 @@
+package com.controlestoque.dto;
+
+public class PedidosMes {
+	
+	private String mes;
+	private Integer total;
+
+	public PedidosMes() {
+
+	}
+
+	public PedidosMes(String mes, Integer total) {
+		super();
+		this.mes = mes;
+		this.total = total;
+	}
+
+	public String getMes() {
+		return mes;
+	}
+
+	public void setMes(String mes) {
+		this.mes = mes;
+	}
+
+	public Integer getTotal() {
+		return total;
+	}
+
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+
+}
