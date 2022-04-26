58e8a1785ce9db07712d1571dc968ab2a441baf2
prodrigues12 <paulinorn12@gmail.com>
2022-04-26 05:51:41 -0300

    Configuração do imprimir relatorio - teste ok

--- controlestoque/src/main/java/com/controlestoque/WebConfig.java
+++ controlestoque/src/main/java/com/controlestoque/WebConfig.java
@@ -1,5 +1,20 @@
 package com.controlestoque;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
 public class WebConfig {
+	
+	
+	@Bean
+	public DataSource dataSource() {
+	    return new EmbeddedDatabaseBuilder()
+	      .setType(EmbeddedDatabaseType.HSQL)
+//	      .addScript("classpath:employee-schema.sql")
+	      .build();
+	}
 
 }
--- controlestoque/src/main/java/com/controlestoque/controller/RelatoriosController.java
+++ controlestoque/src/main/java/com/controlestoque/controller/RelatoriosController.java
@@ -1,9 +1,12 @@
 package com.controlestoque.controller;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,11 +16,11 @@ import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.controlestoque.dto.PeriodoRelatorio;
 
-import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
@@ -26,6 +29,9 @@ import net.sf.jasperreports.engine.JasperPrint;
 @Controller
 @RequestMapping("/relatorios")
 public class RelatoriosController {
+	
+	@Autowired
+	private DataSource dataSource;
 
 //	@GetMapping("/pedidosFinalizados")
 	public ModelAndView reltoriosPedidosFinalizados() {
@@ -53,34 +59,37 @@ public class RelatoriosController {
 
 
 	@GetMapping("/pedidosFinalizados")
-	public ResponseEntity<PeriodoRelatorio> getEmployeeRecordReport() {
+	public ResponseEntity<Object> getEmployeeRecordReport(@RequestParam Map<String, Object> parametros, HttpServletResponse response) {
 
 		try {
 			
-		;
+//			Map<String, Object> paramentros = new HashMap<String, Object>();
+			
+			parametros = parametros == null ? parametros = new HashMap<>() : parametros;
+			
+			parametros.put("status" , "NOVO");
 
-			// dynamic parameters required for report
-			Map<String, Object> empParams = new HashMap<String, Object>();
-			empParams.put("data_inicial" , "01-02-2022");
-			empParams.put("data_final"  , "25-04-2022");
+			
 			JasperPrint empReport = JasperFillManager.fillReport(JasperCompileManager
-					.compileReport(ResourceUtils.getFile("classpath:relatorios/relatorio_pedidosMes.jrxml").getAbsolutePath())																					
-					, empParams
-					, new JREmptyDataSource());
+					.compileReport(ResourceUtils.getFile("classpath:relatorios/relatorio_pedidosFinalizadosSem.jrxml").getAbsolutePath())																					
+					, parametros
+					, dataSource.getConnection());
 
+//			##############
 			HttpHeaders headers = new HttpHeaders();
 			
-			// set the PDF format
 			headers.setContentType(MediaType.APPLICATION_PDF);
 			headers.setContentDispositionFormData("filename", "relatorio_pedidosMes.pdf");
 			
-			// create the report in PDF format
-			return new ResponseEntity<PeriodoRelatorio>(JasperExportManager.exportReportToPdf(empReport), headers, HttpStatus.OK);
+			
+			return new ResponseEntity<Object>(JasperExportManager.exportReportToPdf(empReport), headers, HttpStatus.OK);
 
 		} catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 
 	}
+	
+	
 
 }
