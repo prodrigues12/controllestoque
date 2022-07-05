6350efa0acc325a625a26c45184ef35f5bf125e8
prodrigues12 <paulinorn12@gmail.com>
2022-07-05 05:55:18 -0300

    Tentativa de adicioar o sweetalert ao salvar

--- controlestoque/src/main/resources/static/js/controllestoque.dialogo-salvar.js
+++ controlestoque/src/main/resources/static/js/controllestoque.dialogo-salvar.js
@@ -1,59 +1,24 @@
 var Controllestoque = Controllestoque || {};
 
-Controllestoque.DialogoSalvar = (function() {
 
-	function DialogoSalvar() {
-		this.salvarBtn = $('.js-salvar-btn');
+this.alerta = $('.alert-danger');
 
 
-	}
+if(alerta.isElementPresent){
+console.log('chegou aqui');
 
-	DialogoSalvar.prototype.iniciar = function() {
-		this.salvarBtn.on('click', onSalvarClique.bind(this));
-	}
+ onSalvarClique();
 
-	function onSalvarClique() {
+}
+function onSalvarClique() {
+	console.log('chegou aqui')
+	swal({
+		title: "Good job!",
+		text: "You clicked the button!",
+		icon: "success",
+		timer: 30000
+	});
 
-		var form = document.querySelector("#form-secao");
-//		var inputNome = document.getElementById('nomeSecao');
-		let secao = { nome: form.nomeSecao.value }
-
-		$.ajax({
-			url: "/secao",
-			method: 'POST',
-			contentType: "application/json",
-			data: JSON.stringify(secao),
-			success: chamarModalSucesso(),
-			error: onErroExcluir()
-
-		});
-	}
-	function chamarModalSucesso() {
-		Swal.fire({
-			icon: 'success',
-			timer: 5000,
-			title: 'Salvo com sucesso?',
-			text: 'Deseja realizar outra operação?',
-			footer: '<a href="">Why do I have this issue?</a>'
-		})
-	}
-
-	function onErroExcluir() {
-		Swal.fire({
-			icon: 'error',
-			timer: 5000,
-			title: 'Oops...',
-			text: 'Não foi possivel salvar!',
-			footer: '<a href="">Why do I have this issue?</a>'
-		})
+}
 
 
-	}
-	return DialogoSalvar;
-
-}());
-
-$(function() {
-	var dialogo = new Controllestoque.DialogoSalvar();
-	dialogo.iniciar();
-});
