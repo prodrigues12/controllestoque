82c626f172674662fd7d67839ab84466e64efb71
prodrigues12 <paulinorn12@gmail.com>
2022-07-23 00:56:26 -0300

    ajustes no war

--- controlestoque/src/main/java/com/controlestoque/ControleEstoqueApplication.java
+++ controlestoque/src/main/java/com/controlestoque/ControleEstoqueApplication.java
@@ -2,9 +2,10 @@ package com.controlestoque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ControleEstoqueApplication {
+public class ControleEstoqueApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControleEstoqueApplication.class, args);
