8827447a356232e3d1b9d1aad7e82e93bcc991b6
prodrigues12 <paulinorn12@gmail.com>
2023-09-04 05:57:49 -0300

    Ajuste de pagina / iniciio do dashboard odo financeiro

--- controlestoque/src/main/resources/static/js/sindebar.js
+++ controlestoque/src/main/resources/static/js/sindebar.js
@@ -1,38 +1,36 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function () {
+  const showNavbar = (toggleId, navId, bodyId, headerId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+    const bodypd = document.getElementById(bodyId);
+    const headerpd = document.getElementById(headerId);
 
-	const showNavbar = (toggleId, navId, bodyId, headerId) => {
-		const toggle = document.getElementById(toggleId),
-			nav = document.getElementById(navId),
-			bodypd = document.getElementById(bodyId),
-			headerpd = document.getElementById(headerId)
+    if (toggle && nav && bodypd && headerpd) {
+      toggle.addEventListener('click', () => {
+        nav.classList.toggle('show-barra');
+        toggle.classList.toggle('bx-x');
+        bodypd.classList.toggle('body-pd');
+        headerpd.classList.toggle('body-pd');
+        headerpd.classList.toggle('header-expanded');
 
-		// Validate that all variables exist
-		if (toggle && nav && bodypd && headerpd) {
-			toggle.addEventListener('click', () => {
-				// show navbar
-				nav.classList.toggle('show-barra')
-				// change icon
-				toggle.classList.toggle('bx-x')
-				//				 add padding to body
-				bodypd.classList.toggle('body-pd')
-				// add padding to header
-				headerpd.classList.toggle('body-pd')
-			})
-		}
-	}
+//         Verifica se o header_toggle está ativado e ajusta o tamanho do cabeçalho
+        if (toggle.classList.contains('bx-x')) {
+          headerpd.style.height = '50px'; // Ajuste o valor conforme necessário
+        } else {
+          headerpd.style.height = ''; // Volta ao tamanho original (removendo a altura definida)
+        }
+      });
+    }
+  }
 
-	showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
+  showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header');
 
-	/*===== LINK ACTIVE =====*/
-	const linkColor = document.querySelectorAll('.nav_link')
+  function colorLink() {
+    const linkColor = document.querySelectorAll('.nav_link');
+    linkColor.forEach(link => link.classList.remove('active'));
+    this.classList.add('active');
+  }
 
-	function colorLink() {
-		if (linkColor) {
-			linkColor.forEach(l => l.classList.remove('active'))
-			this.classList.add('active')
-		}
-	}
-	linkColor.forEach(l => l.addEventListener('click', colorLink))
-
-	// Your code to run since DOM is loaded and ready
-});	
+  const linkColor = document.querySelectorAll('.nav_link');
+  linkColor.forEach(link => link.addEventListener('click', colorLink));
+});
